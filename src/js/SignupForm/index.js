a20503f7ed3e616bb0b3cdeccec57590c1b9fccc
Tahmid Sadik <tahmidsadik112@gmail.com>
2017-06-03 20:20:48 +0600

    Adds flow configurations

--- src/js/SignupForm/index.js
+++ src/js/SignupForm/index.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from 'react';
 import Container from './Container';
 import Banner from './Banner';
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SignupForm from './SignupForm';
 // import { AppContainer } from 'react-hot-loader';
 
-import './../styl/signup.styl';
+// unfortunate flow doesn't understand css modules
+// $FlowIgnore
+import '../styl/signup.styl';
 
 const container = document.getElementById('container');
 
