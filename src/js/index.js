8f44cd244b8564ee16e5f97da2faa3e099aeaab5
Tahmid Sadik <tahmidsadik112@gmail.com>
2017-06-03 13:49:52 +0600

    Disables webpack hot reloading

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SignupForm from './SignupForm';
-import { AppContainer } from 'react-hot-loader';
+// import { AppContainer } from 'react-hot-loader';
 
 import './../styl/signup.styl';
 
 const container = document.getElementById('container');
 
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    container);
-};
+ReactDOM.render(<SignupForm />, container);
 
+// const render = (Component) => {
+//   ReactDOM.render(
+//     <AppContainer>
+//       <Component />
+//     </AppContainer>,
+//     container);
+// };
 
-render(SignupForm);
+// render(SignupForm);
 
-if (module.hot) {
-  module.hot.accept('./SignupForm', () => {
-    render(SignupForm);
-  });
-}
+// if (module.hot) {
+//   module.hot.accept('./SignupForm', () => {
+//     render(SignupForm);
+//   });
+// }
